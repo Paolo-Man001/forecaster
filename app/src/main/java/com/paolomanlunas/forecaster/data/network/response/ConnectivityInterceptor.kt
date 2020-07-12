da9f197bf369ae29d854f6b3c6ffdc2bf8c2c7c0
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-07-12 21:09:11 +1200

    Add new Interface - ConnectionInterceptor.

---
+++ app/src/main/java/com/paolomanlunas/forecaster/data/network/response/ConnectivityInterceptor.kt
@@ -0,0 +1,10 @@
+package com.paolomanlunas.forecaster.data.network.response
+
+/* ConnectivityInterceptor
+*  - type of Interceptor that checks if there is a network connection.
+*    It will throw and cache an Exception if there's no network connection.
+*    We can PREVENT app from CRASHING when it doesn't detect a network connection.
+* */
+
+interface ConnectivityInterceptor {
+}
